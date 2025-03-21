0d5e4557f8ea401795ace0967c345b03dd121f14
kush <email@gmail.com>
2025-03-21 09:15:45 +0530

    Make a function to check the type of triangle

--- 4.js
+++ 4.js
@@ -1,1 +1,13 @@
-write 
+// write function which checks the type of triangle (equivalent , isosceles , scalene). 
+
+function triangleType(sideA , sideB , sideC){
+    let statement = `The triangle of side (${sideA} , ${sideB} , ${sideC}) is`; 
+
+    if(sideA == sideB && sideB == sideC) return `${statement} equivalent triangle`; 
+    else if(sideA == sideB || sideB == sideC || sideA == sideC) return `${statement} Isosceles triangle`; 
+    return `${statement} Scalene triangle`;
+}
+
+console.log(triangleType( 3 , 3 , 3)) // Equivalent trianlge
+console.log(triangleType( 3 , 2 , 3)) // Isosceles trianlge
+console.log(triangleType( 1 , 2 , 3)) // Scalene trianlge
