a3139a6f02211282a019064d1b33716da38c95c2
kush <email@gmail.com>
2025-03-21 09:01:32 +0530

    Count the total alphabets in string

--- 3.js
+++ 3.js
@@ -1,13 +1,14 @@
 // write a function called countChar that takes two parameters : a string and a character to count. The function should return the number of times the specified character appears in the string.
-// Function should be case sensetive it should contain both upper case and lower case
+// Function should be case sensetive. 
 
-let str = "Missisipi"; 
+let str = "MIssisipI"; 
 let findLetter = "i"; 
 
 function counterLetter(str , findLetter){
     let counter = 0; 
+      
     for (let i = 0; i < str.length; i++) {
-        if(str[i] === findLetter){
+        if(str[i].toLowerCase() === findLetter.toLowerCase()){
             counter++; 
         }
     }
